b05f04c1a7ced89d528122de6f2922d4e053bbf6
George Matos <georgemattos@gmail.com>
2020-12-23 12:59:37 -0300

    feat: create Usuario and UsuarioDTO

---
+++ src/main/java/com/fortalezasec/firewarning/domain/UsuarioDTO.java
@@ -0,0 +1,57 @@
+package com.fortalezasec.firewarning.domain;
+
+import java.io.Serializable;
+import java.util.Set;
+
+public class UsuarioDTO implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  private Long id;
+  private String nome;
+  private String email;
+  private Set<Tipo> tipo;
+
+  public UsuarioDTO() {
+  }
+
+  public UsuarioDTO(Usuario usuario) {
+    id = usuario.getId();
+    nome = usuario.getNome();
+    email = usuario.getEmail();
+    tipo = usuario.getPerfis();
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public Set<Tipo> getTipo() {
+    return tipo;
+  }
+
+  public void setTipo(Set<Tipo> tipo) {
+    this.tipo = tipo;
+  }
+
+}
