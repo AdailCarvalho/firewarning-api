a2e2041480610987cba10d1f3a6f9fd3f0600d54
George Matos <georgemattos@gmail.com>
2020-12-23 21:31:04 -0300

    feat: add empresa to DBService

--- src/main/java/com/fortalezasec/firewarning/services/DBService.java
+++ src/main/java/com/fortalezasec/firewarning/services/DBService.java
@@ -3,8 +3,12 @@ package com.fortalezasec.firewarning.services;
 import java.util.Arrays;
 import java.util.HashSet;
 
+import com.fortalezasec.firewarning.domain.Empresa;
+import com.fortalezasec.firewarning.domain.EmpresaFavorita;
+import com.fortalezasec.firewarning.domain.NivelPerigo;
 import com.fortalezasec.firewarning.domain.Tipo;
 import com.fortalezasec.firewarning.domain.Usuario;
+import com.fortalezasec.firewarning.repository.EmpresaRepository;
 import com.fortalezasec.firewarning.repository.UsuarioRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,25 +19,40 @@ import org.springframework.stereotype.Service;
 public class DBService {
 
   private UsuarioRepository usuarioRepository;
+  private EmpresaRepository empresaRepository;
 
   @Autowired
   private BCryptPasswordEncoder bcrypt;
 
   @Autowired
-  public DBService(UsuarioRepository usuarioRepository) {
+  public DBService(UsuarioRepository usuarioRepository, EmpresaRepository empresaRepository, EmpresaService empresaService) {
     this.usuarioRepository = usuarioRepository;
+    this.empresaRepository = empresaRepository;
   }
-  
-  public void instatiateTestDatabase() throws Exception {
-    Usuario usuario1 = new Usuario(null, "José Carlos", "josecarlos@etc.br", bcrypt.encode("senha"), new HashSet<Tipo>(Arrays.asList(Tipo.POPULACAO)));
-    Usuario usuario2 = new Usuario(null, "Patrícia Ramos", "pattyr@tal.br", bcrypt.encode("senha"), new HashSet<Tipo>(Arrays.asList(Tipo.POPULACAO, Tipo.ADMIN)));
-    Usuario usuario3 = new Usuario(null, "Zé Cantor", "zecantor@texaco.com", bcrypt.encode("senha"), new HashSet<Tipo>(Arrays.asList(Tipo.ADMIN)));
-    Usuario usuario4 = new Usuario(null, "SYS_SHELL", "sysshell@shell.com", bcrypt.encode("senha"), new HashSet<Tipo>(Arrays.asList(Tipo.SISTEMA)));
 
-    usuarioRepository.save(usuario1);
-    usuarioRepository.save(usuario2);
-    usuarioRepository.save(usuario3);
-    usuarioRepository.save(usuario4);
+  public void instatiateTestDatabase() throws Exception {
+
+    // Gerar Empresas
+    Empresa empresa1 = new Empresa(null, "05.014.725/0001-52", "Shell", "contato@shell.com", "1111-2222",
+        NivelPerigo.OK);
+    Empresa empresa2 = new Empresa(null, "03.021.302/0001-34", "Texaco", "contato@texaco.com", "2222-3333",
+        NivelPerigo.WARNING);
+    Empresa empresa3 = new Empresa(null, "69.855.137/0001-24", "Petrobras", "contato@petrobras.com", "3333-4444",
+        NivelPerigo.DANGER);
+
+    empresaRepository.saveAll(Arrays.asList(empresa1, empresa2, empresa3));
+
+    // Gerar Usuarios do Sistema
+    Usuario usuario1 = new Usuario(null, "José Carlos", "josecarlos@etc.br", bcrypt.encode("senha"),
+        new HashSet<Tipo>(Arrays.asList(Tipo.POPULACAO)), new EmpresaFavorita(null, empresa1.getCnpj(), "Tudo ok!"));
+    Usuario usuario2 = new Usuario(null, "Patrícia Ramos", "pattyr@tal.br", bcrypt.encode("senha"),
+        new HashSet<Tipo>(Arrays.asList(Tipo.POPULACAO, Tipo.ADMIN)), new EmpresaFavorita(null, empresa2.getCnpj(), "Vazamento de óleo..."));
+    Usuario usuario3 = new Usuario(null, "Zé Cantor", "zecantor@texaco.com", bcrypt.encode("senha"),
+        new HashSet<Tipo>(Arrays.asList(Tipo.ADMIN)), new EmpresaFavorita(null, empresa3.getCnpj(), "Foco de incendio!!!"));
+    Usuario usuario4 = new Usuario(null, "SYS_SHELL", "sysshell@shell.com", bcrypt.encode("senha"),
+        new HashSet<Tipo>(Arrays.asList(Tipo.SISTEMA)), new EmpresaFavorita(null, empresa1.getCnpj(), "Foco de incendio na plataforma"));
+
+    usuarioRepository.saveAll(Arrays.asList(usuario1, usuario2, usuario3, usuario4));
   }
 
 }
