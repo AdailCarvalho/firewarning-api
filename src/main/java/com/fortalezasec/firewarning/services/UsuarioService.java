5c0d5701ce0fb8864e8c62c462c3f0cd983a4dca
George Matos <georgemattos@gmail.com>
2020-12-24 22:10:19 -0300

    feat: improve dto convert methods and add insert, getByEmail and getById methods

--- src/main/java/com/fortalezasec/firewarning/services/UsuarioService.java
+++ src/main/java/com/fortalezasec/firewarning/services/UsuarioService.java
@@ -1,18 +1,31 @@
 package com.fortalezasec.firewarning.services;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import com.fortalezasec.firewarning.Utils.CustomValidators;
+import com.fortalezasec.firewarning.domain.Tipo;
 import com.fortalezasec.firewarning.domain.Usuario;
-import com.fortalezasec.firewarning.domain.UsuarioDTO;
+import com.fortalezasec.firewarning.domain.DTOs.UsuarioDTO;
 import com.fortalezasec.firewarning.repository.UsuarioRepository;
+import com.fortalezasec.firewarning.services.Errors.UserAlreadyExistsException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsuarioService {
-  
+
+  @Autowired
+  private BCryptPasswordEncoder bcrypt;
+
+  @Autowired
+  private CustomValidators validators;
+
   private UsuarioRepository repository;
 
   @Autowired
@@ -25,7 +38,7 @@ public class UsuarioService {
     List<UsuarioDTO> usuariosDTO = new ArrayList<>();
     try {
       usuarios = repository.findAll();
-      usuariosDTO = fromDTO(usuarios);
+      usuariosDTO = toDTOs(usuarios);
     } catch (Exception e) {
       System.out.println(e);
     }
@@ -33,7 +46,42 @@ public class UsuarioService {
     return usuariosDTO;
   }
 
-  private List<UsuarioDTO> fromDTO(List<Usuario> usuarios) {
+  public UsuarioDTO getById(Long id) {
+    Usuario usuario = repository.findById(id).get();
+    UsuarioDTO usuarioDTO = toDTO(usuario);
+
+    return usuarioDTO;
+  }
+
+  public Usuario getByEmail(String email) {
+    Usuario usuario = repository.findByEmail(email);
+
+    return usuario;
+  }
+
+  public Usuario insert(Usuario usuario) throws Exception {
+    if (usuario.getEmpresaFavorita() != null)
+      validators.validarCnpj(usuario.getEmpresaFavorita().getCnpj());
+
+    // Ok, usuario não existe ainda
+
+    if (getByEmail(usuario.getEmail()) == null) {
+      Set<Tipo> perfis = new HashSet<>(Arrays.asList(Tipo.POPULACAO));
+      usuario.setPerfis(perfis);
+      usuario.setSenha(bcrypt.encode((usuario.getSenha())));
+      return repository.save(usuario);
+    } else {
+      throw new UserAlreadyExistsException("Já existe um usuário com o email informado");
+    }
+
+  }
+
+  public UsuarioDTO toDTO(Usuario usuario) {
+    UsuarioDTO usuarioDTO = new UsuarioDTO(usuario);
+    return usuarioDTO;
+  }
+
+  public List<UsuarioDTO> toDTOs(List<Usuario> usuarios) {
     List<UsuarioDTO> usuariosDTO = new ArrayList<>();
     usuarios.stream().forEach(u -> {
       usuariosDTO.add(new UsuarioDTO(u));
@@ -41,4 +89,9 @@ public class UsuarioService {
     return usuariosDTO;
   }
 
+  // public Usuario fromDTO(UsuarioNewDTO dto) {
+  // Usuario usuario = new Usuario(null, dto.getNome(), dto.getEmail(),
+  // dto.getSenha(), dto.getTipo(), dto.getEmpresaFavorita());
+  // return usuario;
+  // }
 }
