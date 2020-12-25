83a9ff3c0b6dc6aa9b3a9af13325ad1d7f6f764c
George Matos <georgemattos@gmail.com>
2020-12-25 11:35:33 -0300

    feat: create a FirewarningApplicationContext to serve application based conext info

---
+++ src/main/java/com/fortalezasec/firewarning/Utils/FirewarningApplicatonContext.java
@@ -0,0 +1,20 @@
+package com.fortalezasec.firewarning.Utils;
+
+import com.fortalezasec.firewarning.domain.Usuario;
+import com.fortalezasec.firewarning.services.UsuarioService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FirewarningApplicatonContext {
+  
+  @Autowired
+  private UsuarioService usuarioService;
+
+  public Usuario getUsuarioLogado() {
+    String usuarioLogadoEmail = SecurityContextHolder.getContext().getAuthentication().getName();
+    return usuarioService.getByEmail(usuarioLogadoEmail);
+  }
+}
