ee34faa72eccb7d84506667947e44fdf27bdbac8
George Matos <georgemattos@gmail.com>
2020-12-24 22:05:42 -0300

    feat: add 2 new errors to ExceptionHandler

--- src/main/java/com/fortalezasec/firewarning/resources/Errors/ResourceExceptionHandler.java
+++ src/main/java/com/fortalezasec/firewarning/resources/Errors/ResourceExceptionHandler.java
@@ -4,9 +4,11 @@ import javax.persistence.EntityNotFoundException;
 import javax.servlet.http.HttpServletRequest;
 
 import com.fortalezasec.firewarning.services.Errors.CNPJInvalidoException;
+import com.fortalezasec.firewarning.services.Errors.UserAlreadyExistsException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -33,12 +35,20 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
 
-	// @ExceptionHandler(DataIntegrityException.class)
-	// public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
+	@ExceptionHandler(UserAlreadyExistsException.class)
+	public ResponseEntity<StandardError> entityNotFound(UserAlreadyExistsException e, HttpServletRequest request) {
 
-	// 	StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
-	// 			"Integridade de dados", e.getMessage(), request.getRequestURI());
-	// 	return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
-	// }
+		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.CONFLICT.value(),
+				"Email informado já está cadastrado", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.CONFLICT).body(err);
+	}
+
+	@ExceptionHandler(UsernameNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(UsernameNotFoundException e, HttpServletRequest request) {
+
+		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
+				"Email informado já está cadastrado", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
 
 }
