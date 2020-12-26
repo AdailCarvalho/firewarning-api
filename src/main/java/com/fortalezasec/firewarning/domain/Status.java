38c73daf7f2ceca31bcab0512624f7a3632d367c
George Matos <georgemattos@gmail.com>
2020-12-26 13:34:58 -0300

    feat: improve Status enum to keep cod and descricao and getStatusByDescription method

--- src/main/java/com/fortalezasec/firewarning/domain/Status.java
+++ src/main/java/com/fortalezasec/firewarning/domain/Status.java
@@ -1,5 +1,56 @@
 package com.fortalezasec.firewarning.domain;
 
+import com.fortalezasec.firewarning.services.Errors.TypeDoNotExistsException;
+
 public enum Status {
-  RESOLVIDO, ABERTO
+  ABERTO(1, "ABERTO"), RESOLVIDO(2, "RESOLVIDO");
+
+  private int cod;
+  private String descricao;
+
+  private Status(int cod, String descricao) {
+    this.cod = cod;
+    this.descricao = descricao;
+  }
+
+  public static Status getStatusByDescricao(String descricao) throws TypeDoNotExistsException {
+    switch (descricao) {
+      case "RESOLVIDO":
+        return Status.RESOLVIDO;
+      case "ABERTO":
+        return Status.ABERTO;
+      default:
+        throw new TypeDoNotExistsException("Status não existe ou não informado");
+    }
+  }
+
+  public int getCod() {
+    return cod;
+  }
+
+  public void setCod(int cod) {
+    this.cod = cod;
+  }
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
+
+  public static Tipo toEnum(Integer cod) {
+    if (cod == null) {
+      return null;
+    }
+
+    for (Tipo t : Tipo.values()) {
+      if (t.getCod() == cod)
+        return t;
+    }
+
+    throw new IllegalArgumentException("Código invalido: " + cod);
+
+  }
 }
