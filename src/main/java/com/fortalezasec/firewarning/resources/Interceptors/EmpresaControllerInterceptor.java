9d6d94cc2c330ea6502b7de63abc199bf647f379
George Matos <georgemattos@gmail.com>
2020-12-27 13:11:06 -0300

    chore: add a description comment in the EmpresaControllerInterceptor

--- src/main/java/com/fortalezasec/firewarning/resources/Interceptors/EmpresaControllerInterceptor.java
+++ src/main/java/com/fortalezasec/firewarning/resources/Interceptors/EmpresaControllerInterceptor.java
@@ -11,10 +11,13 @@ public class EmpresaControllerInterceptor implements HandlerInterceptor {
 
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    
+    // Valida o formato da URI de consulta de Incidentes
     if (request.getRequestURI().toString().equals("/empresas/incidentes") && !request.getParameterMap().isEmpty())
       if (request.getParameter("tipo") == null || request.getParameter("valor") == null)
         throw new UriMalFormadaException("Um parametro obrigatório não foi informado");
     return true;
+
   }
 
 }
