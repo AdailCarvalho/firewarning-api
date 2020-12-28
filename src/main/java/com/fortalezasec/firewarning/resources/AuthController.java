9d64be900547d86abdc1af1d186f05ab8ff2a2d3
George Matos <georgemattos@gmail.com>
2020-12-28 16:50:46 -0300

    fix: removing PasswordEncoder from AuthController

--- src/main/java/com/fortalezasec/firewarning/resources/AuthController.java
+++ src/main/java/com/fortalezasec/firewarning/resources/AuthController.java
@@ -18,7 +18,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,9 +28,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
   @Autowired
   AuthenticationManager authenticationManager;
-
-  @Autowired
-  PasswordEncoder encoder;
 
   @Autowired
   JwtUtils jwtUtils;
